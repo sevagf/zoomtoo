bd323ae833498588f2887566755dcea71875b80f
Sevag Frankian <sevagf@gmail.com>
2015-05-11 21:16:34 -0700

    using jquery-boilerplate to commit initial zoom code

--- README.md
+++ README.md
@@ -1,3 +1,141 @@
-# ZoomToo
+# ZoomToo [![Build Status](https://secure.travis-ci.org/sevagf/zoomtoo.svg?branch=master)](https://travis-ci.org/sevagf/zoomtoo) ![Bower Version](https://badge.fury.io/bo/jquery-boilerplate.svg)
 
-ZoomToo is a jQuery plugin to zoom in when hovering over images.
+ZoomToo is a jQuery plugin to show case your images with a magnifying lens when hovering.
+
+## Usage
+
+1. Include jQuery:
+
+	```html
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	```
+
+2. Include ZoomToo code:
+
+	```html
+	<script src="dist/jquery.zoomtoo.min.js"></script>
+	```
+
+3. Call the plugin:
+
+	```javascript
+	$("#element").zoomtoo({
+		magnify: 1
+	});
+	```
+
+## Structure
+
+The basic structure of the project is given in the following way:
+
+```
+├── demo/
+│   └── index.html
+├── dist/
+│   ├── jquery.zoomtoo.js
+│   └── jquery.zoomtoo.min.js
+├── src/
+│   └── jquery.zoomtoo.js
+├── .editorconfig
+├── .gitignore
+├── .jshintrc
+├── .travis.yml
+├── bower.json
+├── CONTRIBUTING.md
+├── Gruntfile.js
+├── package.json
+├── README.md
+└── zoomtoo.jquery.json
+```
+
+#### [demo/](https://github.com/sevagf/zoomtoo/tree/master/demo)
+
+Contains a simple HTML file to demonstrate the plugin.
+
+#### [dist/](https://github.com/sevagf/zoomtoo/tree/master/dist)
+
+This is where the generated files are stored once Grunt runs.
+
+#### [src/](https://github.com/sevagf/zoomtoo/tree/master/src)
+
+Contains the files responsible for the plugin, you can choose between JavaScript or CoffeeScript.
+
+#### [.editorconfig](https://github.com/sevagf/zoomtoo/tree/master/.editorconfig)
+
+This file is for unifying the coding style for different editors and IDEs.
+
+> Check [editorconfig.org](http://editorconfig.org) if you haven't heard about this project yet.
+
+#### [.gitignore](https://github.com/sevagf/zoomtoo/tree/master/.gitignore)
+
+List of files that we don't want Git to track.
+
+> Check this [Git Ignoring Files Guide](https://help.github.com/articles/ignoring-files) for more details.
+
+#### [.jshintrc](https://github.com/sevagf/zoomtoo/tree/master/.jshintrc)
+
+List of rules used by JSHint to detect errors and potential problems in JavaScript.
+
+> Check [jshint.com](http://jshint.com/about/) if you haven't heard about this project yet.
+
+#### [.travis.yml](https://github.com/sevagf/zoomtoo/tree/master/.travis.yml)
+
+Definitions for continous integration using Travis.
+
+> Check [travis-ci.org](http://about.travis-ci.org/) if you haven't heard about this project yet.
+
+#### [zoomtoo.jquery.json](https://github.com/sevagf/zoomtoo/tree/master/zoomtoo.jquery.json)
+
+Package manifest file used to publish plugins in jQuery Plugin Registry.
+
+> Check this [Package Manifest Guide](http://plugins.jquery.com/docs/package-manifest/) for more details.
+
+#### [Gruntfile.js](https://github.com/sevagf/zoomtoo/tree/master/Gruntfile.js)
+
+Contains all automated tasks using Grunt.
+
+> Check [gruntjs.com](http://gruntjs.com) if you haven't heard about this project yet.
+
+#### [package.json](https://github.com/sevagf/zoomtoo/tree/master/package.json)
+
+Specify all dependencies loaded via Node.JS.
+
+> Check [NPM](https://npmjs.org/doc/json.html) for more details.
+
+## Guides
+
+#### How did we get here?
+
+Have you got in this repo and still not sure about using this boilerplate?
+
+Well, extending jQuery with plugins and methods is very powerful and can save you and your peers a lot of development time by abstracting your most clever functions into plugins.
+
+[This awesome guide](https://github.com/sevagf/zoomtoo/wiki/How-did-we-get-here%3F), adapted from [jQuery Plugins/Authoring](http://docs.jquery.com/Plugins/Authoring), will outline the basics, best practices, and common pitfalls to watch out for as you begin writing your plugin.
+
+#### How to publish plugins?
+
+Also, check our guide on [How to publish a plugin in jQuery Plugin Registry](https://github.com/sevagf/zoomtoo/wiki/How-to-publish-a-plugin-in-jQuery-Plugin-Registry
+)!
+
+## Team
+
+jQuery Boilerplate was made with love by these guys and a bunch of awesome [contributors](https://github.com/sevagf/zoomtoo/graphs/contributors).
+
+[![Zeno Rocha](http://gravatar.com/avatar/e190023b66e2b8aa73a842b106920c93?s=70)](http://zenorocha.com) | [![Addy Osmani](http://gravatar.com/avatar/96270e4c3e5e9806cf7245475c00b275?s=70)](http://addyosmani.com) | [![Helder Santana](http://gravatar.com/avatar/63fb620ee7d14fc91030d4349d189b3e?s=70)](http://heldr.com)
+--- | --- | --- | --- | --- | --- | ---
+[Zeno Rocha](http://zenorocha.com) | [Addy Osmani](http://addyosmani.com) | [Helder Santana](http://heldr.com)
+
+## Contributing
+
+Check [CONTRIBUTING.md](https://github.com/sevagf/zoomtoo/blob/master/CONTRIBUTING.md) for more information.
+
+## History
+
+Check [Releases](https://github.com/sevagf/zoomtoo/releases) for detailed changelog.
+
+## License
+
+[MIT License](http://sevagf.mit-license.org/) © Sevag Frankian
+
+## License
+Copyright (c) 2015 Sevag Frankian Licensed under the MIT license.
---
+++ dist/jquery.zoomtoo.js
@@ -0,0 +1,212 @@
+/*
+ *  jQuery ZoomToo - v0.9.0
+ *  Showcase your images with a magnifying lens on hover
+ *  https://github.com/sevagf/zoomtoo
+ *
+ *  Made by Sevag Frankian
+ *  Under MIT License
+ */
+(function ($) {
+
+  function ZoomToo(element, options) {
+    var zoomToo = this;
+
+    zoomToo.element = element;
+    zoomToo.load(options);
+  }
+
+  ZoomToo.defaults = {
+    showDuration: 500,
+    moveDuration: 1200,
+    magnify: 1,
+    lensWidth: 200,
+    lensHeight: 200
+  };
+
+  ZoomToo.prototype = {
+    load: function(options) {
+      var zoomToo = this,
+        nestedImage = zoomToo.element.find("img").first(),
+        img_src = nestedImage.data("src");
+      zoomToo.element.one("zoomtoo.destroy", $.proxy(zoomToo.destroy, zoomToo));
+      if (!img_src) {
+        return;
+      }
+      zoomToo.img = new Image();
+      zoomToo.img.src = img_src;
+      zoomToo.img.onload = $.proxy(zoomToo.init, zoomToo, options);
+    },
+    init: function(options) {
+      var zoomToo = this,
+        position = zoomToo.element.css("position");
+
+      zoomToo.settings = $.extend({}, ZoomToo.defaults, options);
+
+      zoomToo.element.get(0).style.position = /(absolute|fixed)/.test(position) ? position : "relative";
+      zoomToo.element.get(0).style.overflow = "hidden";
+
+      zoomToo.elementWidth = zoomToo.element.outerWidth();
+      zoomToo.elementHeight = zoomToo.element.outerHeight();
+
+      zoomToo.imgWidth = zoomToo.img.width * zoomToo.settings.magnify;
+      zoomToo.imgHeight = zoomToo.img.height * zoomToo.settings.magnify;
+
+      zoomToo.elementOffset = zoomToo.element.offset();
+
+      zoomToo.newZoom = { left: 0, top: 0 };
+      zoomToo.currentZoom = { left: 0, top: 0 };
+      zoomToo.moveImageTimer = 0;
+      zoomToo.continueSlowMove = false;
+
+      $(zoomToo.img)
+        .css({
+          position: "absolute",
+          top: 0,
+          left: 0,
+          opacity: 0,
+          width: zoomToo.imgWidth,
+          height: zoomToo.imgHeight,
+          border: "none",
+          maxWidth: "none",
+          maxHeight: "none"
+        })
+        .appendTo(zoomToo.element);
+
+      zoomToo.element
+        .css({ cursor: "crosshair" })
+        .on("mouseenter.zoomtoo", $.proxy(zoomToo.mouseEnter, zoomToo))
+        .on("mouseleave.zoomtoo", $.proxy(zoomToo.mouseLeave, zoomToo))
+        .on("mousemove.zoomtoo", $.proxy(zoomToo.mouseMove, zoomToo));
+    },
+    destroy: function() {
+      var zoomToo = this;
+      zoomToo.cancelTimer();
+      zoomToo.element.off();
+      $(zoomToo.img).remove();
+      zoomToo.element.removeData("zoomtoo");
+    },
+    calculateOffset: function(currentMousePos) {
+      var zoomToo = this;
+
+      var currentMouseOffsetX = currentMousePos.x - zoomToo.elementOffset.left,
+        currentMouseOffsetY = currentMousePos.y - zoomToo.elementOffset.top,
+        halfLensHeight = Math.round(zoomToo.settings.lensHeight / 2),
+        halfLensWidth = Math.round(zoomToo.settings.lensWidth / 2),
+        lensTop = currentMouseOffsetY - halfLensHeight,
+        lensBottom = currentMouseOffsetY + halfLensHeight,
+        lensLeft = currentMouseOffsetX - halfLensWidth,
+        lensRight = currentMouseOffsetX + halfLensWidth;
+
+      if (lensTop < 0) {
+        currentMouseOffsetY = halfLensHeight;
+      }
+      if (lensBottom > zoomToo.elementHeight) {
+        currentMouseOffsetY = zoomToo.elementHeight - halfLensHeight;
+      }
+      if (lensLeft < 0) {
+        currentMouseOffsetX = halfLensWidth;
+      }
+      if (lensRight > zoomToo.elementWidth) {
+        currentMouseOffsetX = zoomToo.elementWidth - halfLensWidth;
+      }
+
+      var deltaHeight = zoomToo.imgHeight - zoomToo.elementHeight;
+      var adjustedHeight = zoomToo.elementHeight - zoomToo.settings.lensHeight;
+
+      var deltaWidth = zoomToo.imgWidth - zoomToo.elementWidth;
+      var adjustedWidth = zoomToo.elementWidth - zoomToo.settings.lensWidth;
+
+      var zoomTop = (-deltaHeight/adjustedHeight) * (currentMouseOffsetY - halfLensHeight);
+      var zoomLeft = (-deltaWidth/adjustedWidth) * (currentMouseOffsetX - halfLensWidth);
+
+      zoomToo.newZoom.left = zoomLeft;
+      zoomToo.newZoom.top = zoomTop;
+    },
+    cancelTimer: function() {
+      var zoomToo = this;
+      clearTimeout(zoomToo.moveImageTimer);
+    },
+    stopSlowMoveImage: function() {
+      var zoomToo = this;
+      zoomToo.continueSlowMove = false;
+    },
+    mouseLeave: function() {
+      var zoomToo = this;
+      $(zoomToo.img).stop().fadeTo(zoomToo.settings.showDuration, 0).promise().done(zoomToo.stopSlowMoveImage);
+    },
+    mouseEnter: function(e) {
+      var zoomToo = this,
+        currentMousePos = {
+          x: e.pageX,
+          y: e.pageY
+        };
+      zoomToo.calculateOffset(currentMousePos);
+      zoomToo.continueSlowMove = true;
+      zoomToo.currentZoom.top = zoomToo.newZoom.top;
+      zoomToo.currentZoom.left = zoomToo.newZoom.left;
+      zoomToo.moveImage();
+      $(zoomToo.img).stop().fadeTo(zoomToo.settings.showDuration, 1);
+    },
+    mouseMove: function (e) {
+      var zoomToo = this,
+        currentMousePos = {
+          x: e.pageX,
+          y: e.pageY
+        };
+      zoomToo.calculateOffset(currentMousePos);
+      zoomToo.cancelTimer();
+      zoomToo.continueSlowMove = true;
+      zoomToo.slowMoveImage();
+    },
+    slowMoveImage: function () {
+      var zoomToo = this,
+        delta = { left: 0, top: 0 },
+        moveZoomPos = { left: 0, top: 0},
+        reachedLeft = false,
+        reachedTop = false;
+
+      delta.left = zoomToo.newZoom.left - zoomToo.currentZoom.left;
+      delta.top = zoomToo.newZoom.top - zoomToo.currentZoom.top;
+
+      moveZoomPos.left = -delta.left / (zoomToo.settings.moveDuration / 100);
+      moveZoomPos.top = -delta.top / (zoomToo.settings.moveDuration / 100);
+
+      zoomToo.currentZoom.left = zoomToo.currentZoom.left - moveZoomPos.left;
+      zoomToo.currentZoom.top = zoomToo.currentZoom.top - moveZoomPos.top;
+
+      if (Math.abs(delta.left) < 1) {
+        zoomToo.currentZoom.left = zoomToo.newZoom.left;
+        reachedLeft = true;
+      }
+      if (Math.abs(delta.top) < 1) {
+        zoomToo.currentZoom.top = zoomToo.newZoom.top;
+        reachedTop = true;
+      }
+
+      zoomToo.moveImage();
+
+      if (reachedLeft && reachedTop) {
+        zoomToo.continueSlowMove = false;
+      }
+
+      if (zoomToo.continueSlowMove === true) {
+        zoomToo.moveImageTimer = setTimeout($.proxy(zoomToo.slowMoveImage, zoomToo), 25);
+      }
+    },
+    moveImage: function() {
+      var zoomToo = this;
+      zoomToo.img.style.left = (zoomToo.currentZoom.left) + "px";
+      zoomToo.img.style.top = (zoomToo.currentZoom.top) + "px";
+    }
+  };
+
+  $.fn.zoomToo = function (options) {
+    this.each(function () {
+      var instance = $.data(this, "zoomtoo");
+      if (!instance) {
+        $.data(this, "zoomtoo", new ZoomToo($(this), options));
+      }
+    });
+  };
+
+}(window.jQuery));
